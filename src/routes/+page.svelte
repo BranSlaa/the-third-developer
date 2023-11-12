153389c4f528905472c61f4ce7e117e15f0823b6
Brandon Slaa <branslaa@gmail.com>
2023-11-12 07:57:11 -0600

    main layout complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,210 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class='relative bg-gray-100 hero-pattern py-64 overflow-hidden'>
+	<img src='https://picsum.photos/800' alt='' class='absolute object-cover w-full h-full top-0 left-0'/>
+	<div class="bg-gradient-to-r from-black to-transparent absolute w-full h-full top-0 left-0"></div>
+	<section id='hero' class='container mx-auto flex items-center relative z-10'>
+		<div class='w-1/2'>
+			<h1 class='text-white text-7xl leading-snug'>The Third Developer</h1>
+			<p class='text-gray-400 mt-4'>The first developer focuses on the problem. The second developer focuses on the solution. I look through the surface to peer into the true meaning. I am the Third Developer, finding answers that best fit your needs.</p>
+			<a href='#pricing' class='bg-gray-700 text-white px-6 py-2 block w-fit mt-6 rounded uppercase text-sm'>Lets begin</a>
+		</div>
+	</section>
+</div>
+<section id='about' class='container mx-auto flex gap-8 items-center'>
+	<div class='w-1/2'>
+		<h2 class='text-5xl font-bold mb-4'>Who am I?</h2>
+		<p class='mb-8 text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.</p>
+		<div class='flex'>
+			<div class='point'>
+				<img class='mb-2' src='https://picsum.photos/100' alt=''/>
+				<h3 class='mb-1'>Wisdom</h3>
+				<p class='text-gray-400 leading-tight text-sm'>Lorem ipsum dolor sit amet, consectetur</p>
+			</div>
+			<div class='point'>
+				<img class='mb-2' src='https://picsum.photos/100' alt=''/>
+				<h3 class='mb-1'>Knowledge</h3>
+				<p class='text-gray-400 leading-tight text-sm'>Lorem ipsum dolor sit amet, consectetur</p>
+			</div>
+			<div class='point'>
+				<img class='mb-2' src='https://picsum.photos/100' alt=''/>
+				<h3 class='mb-1'>Truth</h3>
+				<p class='text-gray-400 leading-tight text-sm'>Lorem ipsum dolor sit amet, consectetur</p>
+			</div>
+		</div>
+	</div>
+	<div class='w-1/2 py-24'>
+		<img src='https://picsum.photos/400' alt='' class='w-full'/>
+	</div>
+</section>
+<div class='container mx-auto px-16 py-24'>
+	<section id='process' class='flex gap-8'>
+		<div class='w-1/2'>
+			<h2 class='text-5xl font-bold'>How it Works</h2>
+			<h3 class='mt-12 text-2xl text-gray-400 font-medium'>It's insanely easy to get started</h3>
+			<p class='mt-2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+			<a href='#pricing' class='bg-gray-700 text-white px-6 py-2 block w-fit mt-16 rounded uppercase text-sm'>Start Now</a>
+		</div>
+		<ul class='w-1/2 flex flex-col gap-12'>
+			<li>
+				<h3 class='font-medium text-2xl mb-4'>Concept</h3>
+				<p class='text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+			</li>
+			<li>
+				<h3 class='font-medium text-2xl mb-4'>Design</h3>
+				<p class='text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+			</li>
+			<li>
+				<h3 class='font-medium text-2xl mb-4'>Development</h3>
+				<p class='text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+			</li>
+		</ul>
+	</section>
+</div>
+<div class='bg-gray-100'>
+	<section id='pricing' class='container mx-auto px-16 py-24'>
+		<h2 class='text-5xl font-bold mb-2 text-center'>Pricing</h2>
+		<p class='text-2xl text-gray-400 font-medium text-center'>Select the plan that best suits your needs</p>
+		<div class='flex mt-16 gap-6'>
+			<div class='w-1/3 p-12'>
+				<h3 class='text-center text-gray-400 font-medium uppercase text-2xl mb-4'>Brochure</h3>
+				<p class='text-center text-xs text-gray-400 mb-1'>starting at</p>
+				<h4 class='text-center font-bold text-5xl'>$400</h4>
+				<p class='text-center mt-8 mb-6 pb-6 border-b border-b-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+				<div class='flex flex-col items-center w-full'>
+					<p class='text-l uppercase text-gray-700'>options</p>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Pages</strong>
+						<p class='flex-1 text-gray-400'>1+</p>
+					</div>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Many</strong>
+						<p class='flex-1 text-gray-400'>CMS Options</p>
+					</div>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Multiple</strong>
+						<p class='flex-1 text-gray-400'>Design Options</p>
+					</div>
+				</div>
+				<a href='#contact' class='border border-gray-700 px-6 py-2 block w-fit mx-auto mt-8 rounded uppercase text-sm'>Choose</a>
+			</div>
+			<div class='w-1/3 p-12 bg-white'>
+				<h3 class='text-center text-gray-400 font-medium uppercase text-2xl mb-4'>Ecommerce</h3>
+				<p class='text-center text-xs text-gray-400 mb-1'>starting at</p>
+				<h4 class='text-center font-bold text-5xl'>$3000</h4>
+				<p class='text-center mt-8 mb-6 pb-6 border-b border-b-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+				<div class='flex flex-col items-center w-full'>
+					<p class='text-l uppercase text-gray-700'>options</p>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Pages</strong>
+						<p class='flex-1 text-gray-400'>5+</p>
+					</div>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Many</strong>
+						<p class='flex-1 text-gray-400'>Payment Processes</p>
+					</div>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Multiple</strong>
+						<p class='flex-1 text-gray-400'>Design Options</p>
+					</div>
+				</div>
+				<a href='#contact' class='bg-gray-700 text-white px-6 py-2 block w-fit mx-auto mt-8 rounded uppercase text-sm'>Choose</a>
+			</div>
+			<div class='w-1/3 p-12'>
+				<h3 class='text-center text-gray-400 font-medium uppercase text-2xl mb-4'>Blog</h3>
+				<p class='text-center text-xs text-gray-400 mb-1'>starting at</p>
+				<h4 class='text-center font-bold text-5xl'>$1000</h4>
+				<p class='text-center mt-8 mb-6 pb-6 border-b border-b-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+				<div class='flex flex-col items-center w-full'>
+					<p class='text-l uppercase text-gray-700'>options</p>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Pages</strong>
+						<p class='flex-1 text-gray-400'>3+</p>
+					</div>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Many</strong>
+						<p class='flex-1 text-gray-400'>CMS Options</p>
+					</div>
+					<div class='flex w-full gap-x-4 mt-2'>
+						<strong class='flex-1 text-right'>Multiple</strong>
+						<p class='flex-1 text-gray-400'>Design Options</p>
+					</div>
+				</div>
+				<a href='#contact' class='border border-gray-700 px-6 py-2 block w-fit mx-auto mt-8 rounded uppercase text-sm'>Choose</a>
+			</div>
+		</div>
+	</section>
+</div>
+<div class='container mx-auto'>
+	<section id='portfolio' class='portfolio-section py-24'>
+		<h2 class='text-5xl font-bold text-center mb-24'>New Work</h2>
+		<ul class='flex border border-gray-700 rounded w-fit mx-auto mb-24 p-1'>
+			<li class='py-2 px-8 rounded bg-gray-700 text-white'>All</li>
+			<li class='py-2 px-8 rounded hover:bg-gray-700 hover:text-white'>Brochure</li>
+			<li class='py-2 px-8 rounded hover:bg-gray-700 hover:text-white'>Ecommerce</li>
+			<li class='py-2 px-8 rounded hover:bg-gray-700 hover:text-white'>Blog</li>
+		</ul>
+		<div class='grid grid-cols-3 gap-8'>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+			<img class='w-full' src='https://picsum.photos/200' alt=''/>
+		</div>
+	</section>
+</div>
+<section id='testimonials' class='bg-gray-100'>
+	<div class='container mx-auto py-32 max-w-4xl'>
+		<h2 class='text-5xl font-bold text-center mb-16'>Testimonials</h2>
+		<div class='flex gap-16 mt-8'>
+			<img src='https://picsum.photos/100' alt=''/>
+			<div class='content-wrapper'>
+				<p class='text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat labore et dolore.</p>
+				<div class='flex gap-4 mt-4'>
+					<h3 class='attestant'>Bruce Wayne</h3>
+					<h4 class='text-gray-400'>Escentric Billionaire</h4>
+				</div>
+			</div>
+		</div>
+		<div class='flex gap-16 mt-8'>
+			<img src='https://picsum.photos/100' alt=''/>
+			<div class='content-wrapper'>
+				<p class='text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat labore et dolore.</p>
+				<div class='flex gap-4 mt-4'>
+					<h3 class='attestant'>Barry Allen</h3>
+					<h4 class='text-gray-400'>Athlete</h4>
+				</div>
+			</div>
+		</div>
+		<div class='flex gap-16 mt-8'>
+			<img src='https://picsum.photos/100' alt=''/>
+			<div class='content-wrapper'>
+				<p class='text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat labore et dolore.</p>
+				<div class='flex gap-4 mt-4'>
+					<h3 class='attestant'>Clark Kent</h3>
+					<h4 class='text-gray-400'>News Reporter</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id='contact' class='container mx-auto py-16 max-w-5xl'>
+	<h2 class='text-5xl font-bold text-center mb-16'>Contact Brandon</h2>
+	<div class='flex gap-8 items-center'>
+		<div class='w-1/2'>
+			<h3 class='text-2xl font-medium mb-4'>I'd love to hear from you</h3>
+			<p class='text-gray-400 mb-8'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href='mailto:branslaa@gmail.com' class='text-gray-400 text-xl'>branslaa@gmail.com</a>
+		</div>
+		<form class='w-1/2 flex flex-col gap-4'>
+			<input type='email' placeholder="email address" class='border border-gray-400 p-2' />
+			<select name='' id='' class='border border-gray-400 p-2'>
+				<option>value</option>
+			</select>
+			<textarea name='' id='' cols='30' rows='5' class='border border-gray-400 p-2'></textarea>
+			<button class='bg-gray-700 text-white px-6 py-2 block w-full mt-4 mx-auto rounded uppercase text-sm'>Submit</button>
+		</form>
+	</div>
+</section>
