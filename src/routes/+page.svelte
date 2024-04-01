74e3b4ddb04625413572e2183bb5bf9a8fe2d295
Brandon Slaa <branslaa@gmail.com>
2024-04-01 14:30:10 -0400

    google analyitics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<!-- <div class="bg-gradient-to-b from-blue-950 to-blue-800 absolute -z-10 w-full h-full top-0 left-0" /> -->
 	<section id="hero" class="container mx-auto flex justify-center items-center text-center relative z-10">
 		<div class="max-w-4xl">
-			<h1 class="">
+			<h1>
 				<span class="block text-7xl leading-none font-header font-bold text-blue-800">Brandon Slaa</span>
 				<div class="relative h-4">
 					<span class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-9xl leading-none font-cursive text-yellow-500 drop-shadow-lg z-10">is</span>
@@ -10,22 +10,23 @@
 				<span class="block text-5xl leading-none font-header font-bold text-blue-800">the Third Developer</span>
 			</h1>
 			<p class="text-blue-900 mt-4 max-w-2xl mx-auto">
-				The first developer focuses on the problem. The second developer focuses on the solution.<br/>I
-				look through the surface to peer into the true meaning. I am the Third Developer, finding
-				answers that best fit your needs.
+				The first developer focuses on the problem.<br>
+				The second developer focuses on the solution.<br/>
+				I look through the surface to peer into the true meaning.<br>
+				I am the Third Developer, finding answers that best fit your needs.
 			</p>
 		</div>
 	</section>
 	<div class='container mx-auto max-w-xl mt-16 flex gap-8 flex-col justify-center items-center relative z-10'>
-		<div class=''>
+		<div>
 			<h2 class='relative text-3xl w-fit font-header font-bold text-blue-800 mb-2 after:content-[""] after:absolute after:bottom-0 after:left-0 after:h-4 after:w-full after:bg-yellow-500 after:-z-10'>Website Development</h2>
 			<p class="text-blue-900">With a wealth of experience gained from London's top agencies, I've honed my skills in website development, catering to a diverse clientele ranging from local startups to global enterprises. My approach emphasizes not only creating visually captivating websites but also ensuring they are accessible to all users and fully optimized for seamless operation on mobile devices. By prioritizing accessibility standards and employing responsive design techniques, I ensure that your website reaches a wider audience and delivers an exceptional user experience across all devices.</p>
 		</div>
-		<div class=''>
+		<div>
 			<h2 class='relative text-3xl w-fit font-header font-bold text-blue-800 mb-2 after:content-[""] after:absolute after:bottom-0 after:left-0 after:h-4 after:w-full after:bg-yellow-500 after:-z-10'>Process Automation</h2>
 			<p class="text-blue-900">Utilizing cutting-edge technologies and customized solutions, I specialize in streamlining your workflows through process automation. From repetitive tasks to complex procedures, I develop automated systems tailored to your specific needs, allowing you to save time, reduce errors, and focus on what matters most - growing your business.</p>
 		</div>
-		<div class=''>
+		<div>
 			<h2 class='relative text-3xl w-fit font-header font-bold text-blue-800 mb-2 after:content-[""] after:absolute after:bottom-0 after:left-0 after:h-4 after:w-full after:bg-yellow-500 after:-z-10'>Consultation</h2>
 			<p class="text-blue-900">Embarking on your journey to establish a powerful online presence can be daunting. That's where I come in. As a dedicated web development professional, I offer comprehensive consultation services tailored to meet the unique needs of your business. Whether you're a budding startup or a growing enterprise, I provide personalized guidance and strategic insights to help you navigate the complexities of website development. From defining your project goals to selecting the right technologies and refining your user experience, I'll be there every step of the way to ensure your vision becomes a reality. Let's collaborate to build a website that not only captures your brand essence but also drives tangible results for your business.</p>
 		</div>
@@ -33,7 +34,7 @@
 	<div class="container mx-auto pt-16">
 		<section id="portfolio" class="portfolio-section">
 			<h2 class="text-5xl text-blue-800 font-bold text-center mb-8">Portfolio</h2>
-			<div class="grid grid-cols-3 gap-8">
+			<div class="grid md:grid-cols-3 gap-8 grid">
 				<a href='https://runkeeper.com/cms/'>
 					<img class="w-full rounded-lg border-4 border-yellow-500 hover:shadow-lg" src="/asics-runkeeper.webp" alt="ASICS Runkeeper" />
 					<caption class='block text-blue-900 font-header font-bold mt-2 text-xl w-full text-center'>ASICS Runkeeper</caption>
