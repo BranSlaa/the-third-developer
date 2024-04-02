8b8f4209e9c0b5c4c04165a67c007c21edc01264
Brandon Slaa <branslaa@gmail.com>
2024-04-02 04:07:07 -0400

    copy

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
 			cursive: ['Licorice', 'cursive']
 		},
 		dropShadow: {
-			yeller: '0px 2px 1px rgb(0 0 0 / 0.3)'
+			yeller: '1px 0px 0px rgb(0 0 0 / 0.5)'
 		}
 	},
 	plugins: []
